01aeefd0fa36622c51c377c9da5c816fa2a26efe
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-10 07:14:50 -0500

    Complete challenges 1-9 and 1-10.

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,27 @@
-function countVowels() {}
+//use regex to determine if a character is a vowel
+// add all vowel characters to an array
+// return length of that array
+
+// function countVowels(str) {
+//   // make string lowercase
+//   const formatStr = str.toLowerCase();
+//   //   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   const vowels = str.match(/[aeiou]/gi);
+
+//   return vowels ? vowels.length : 0;
+// }
+
+//using a for loop
+function countVowels(str) {
+  // make string lowercase
+  const formatStr = str.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < formatStr.length; i++) {
+    if (formatStr[i].match(/[aeiou]/gi)) {
+      count += 1;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,20 @@
-function removeDuplicates() {}
+// function removeDuplicates(array) {
+//   // create a new array to store unique values
+//   const uniqueArray = [];
+//   // loop through the array
+//   for (let i = 0; i < array.length; i++) {
+//     // if the unique array does not include the current value
+//     if (!uniqueArray.includes(array[i])) {
+//       // push the value to the unique array
+//       uniqueArray.push(array[i]);
+//     }
+//   }
+//   // return the unique array
+//   return uniqueArray;
+// }
+
+function removeDuplicates(array) {
+  return Array.from(new Set(array));
+}
 
 module.exports = removeDuplicates;
