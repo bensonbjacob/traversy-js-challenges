c9161464272225e795f4eb65609662f95c663121
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-13 16:06:39 -0500

    Complete challenges 2-6 and 2-7

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,12 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const count = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (count.has(str[i])) {
+      return false;
+    }
+    count.add(str[i]);
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charMap = {};
+  // create a map of characters and their count
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+    // charMap[char] = (charMap[char] || 0) + 1;
+  }
+  //
+  for (let char of str) {
+    if (charMap[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
