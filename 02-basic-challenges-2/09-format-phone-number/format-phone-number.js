49bc7c1f855c341c273501bd8b57c5343055d719
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-17 11:13:28 -0500

    Complete challenges 2-9 and 2-10

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,27 @@
-function formatPhoneNumber() {}
+// First method
+// function formatPhoneNumber(numbers) {
+//   const areaCode = numbers.slice(0, 3).join('');
+//   const prefix = numbers.slice(3, 6).join('');
+//   const lineNumber = numbers.slice(6).join('');
+//   return `(${areaCode}) ${prefix}-${lineNumber}`;
+// }
+
+//Second method
+// function formatPhoneNumber(numbers) {
+//   const formattedNumbers = numbers.join('');
+//   return `(${formattedNumbers.substring(
+//     0,
+//     3
+//   )}) ${formattedNumbers.substring(
+//     3,
+//     6
+//   )}-${formattedNumbers.substring(6)}`;
+// }
+
+//Third method
+const formatPhoneNumber = (numbers) =>
+  `(${numbers.slice(0, 3).join('')}) ${numbers
+    .slice(3, 6)
+    .join('')}-${numbers.slice(6).join('')}`;
 
 module.exports = formatPhoneNumber;
--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,25 @@
-function validateEmail() {}
+//Using Regex
+// function validateEmail(email) {
+//   const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+//   return regex.test(email);
+// }
+
+function validateEmail(email) {
+  if (email.indexOf('@') < 0) {
+    return false;
+  }
+
+  const [local, domain] = email.split('@');
+  if (local.length === 0 || domain.length < 3) {
+    return false;
+  }
+  const domainExtension = domain.split('.');
+  if (domainExtension.length < 2 || domainExtension[1] < 2) {
+    return false;
+  }
+  {
+    return true;
+  }
+}
 
 module.exports = validateEmail;
