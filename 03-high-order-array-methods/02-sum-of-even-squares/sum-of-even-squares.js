26b1404aab1cd3c30b780ba9d2ee691ae393d50e
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-18 08:26:36 -0500

    Complete challenge 3-2

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,9 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNums = numbers
+    .filter((num) => num % 2 === 0)
+    .map((num) => num ** 2)
+    .reduce((total, num) => total + num, 0);
+  return evenNums;
+}
 
 module.exports = sumOfEvenSquares;
