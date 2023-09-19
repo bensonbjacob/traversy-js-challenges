c1623a203761b20143ece063d53ec54af0100304
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-18 19:04:06 -0500

    Complete challenge 3-3

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,8 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalSales = products.reduce((total, product) => {
+    return total + product.price * product.quantity;
+  }, 0);
+  return totalSales * (1 + taxRate / 100);
+}
 
 module.exports = calculateTotalSalesWithTax;
